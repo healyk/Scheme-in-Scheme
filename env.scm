f007efd702948a6ab3548cf75433135dd41e19d6
Kris Healy <healyk@gmail.com>
2011-02-25 22:29:45 -0700

    Simplifing the defines get/add

--- env.scm
+++ env.scm
@@ -14,24 +14,18 @@
 
 ;; Makes a new environment
 (define (make-env)
-  '((defs )))
+  '((defs ((placeholder 123)))))
 
 ;; Adds a new symbol to the environment with the given form.
 (define (env-add-def env symbol form)
-  (replace env 'defs 
+  (replace env 'defs
 	   (append 
 	    (cdr (assq 'defs env))
 	    (list (list symbol form)))))
 
-(define (env-get-defs env)
-  (let ((defs (assq 'defs env)))
-    (if (equal? (length defs) 1)
-        '()
-        (cadr defs))))
-
 ;; Gets the define for symbol in environment env
 (define (env-get-def env symbol)
-  (let ((form (assq symbol (env-get-defs env))))
+  (let ((form (assq symbol (cadr (assq 'defs env)))))
     (if form
         (cadr form)
         'error)))
