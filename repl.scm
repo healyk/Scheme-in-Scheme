380b3512107f806a95127cc88f1dc252aa2c56db
Kris Healy <healyk@gmail.com>
2011-03-27 10:48:33 -0700

    Execution of arithmetic with arbitrary-length arguments.  Moved some code from repl to vm

--- repl.scm
+++ repl.scm
@@ -14,43 +14,6 @@
 (define (read-input)
   (read))
 
-;; Executes a form.  Input is expected to be a list with the executable symbol
-;; being the first element.  All other elements in input are evaluated and
-;; placed on the environment stack.
-(define (exec-form input env)
-  (for-each (lambda (x) (exec-instruction! env 'push (vm-eval x env)))
-	    (cdr input))
-  (exec-instruction! env (car input))
-  (exec-instruction! env 'pop))
-
-;; Stolen from: http://pointlessprogramming.wordpress.com/2011/02/24/lispy-in-chicken-self-evaluating-values/
-(define (self-evaulating? expr)
-  (or (string? expr)
-      (number? expr)
-      (null? expr)
-      (char? expr)
-      (boolean? expr)))
-
-;; Evaluates an input string
-(define (vm-eval input env)
-  (cond
-   ; Semi-hack used to exit the repl
-   [(and (symbol? input)
-	 (equal? input 'quit)) (values 'quit env)]   
-   [(self-evaulating? input) (values input env)]
-   [(symbol? input)  (values (env/get-def env input) env)]
-   
-   ; Process define forms
-   [(and (list? input)
-         (define-form? input))
-    (define-form input env)]
-
-   ; Process execution of lists
-   [(and (list? input)
-	 (instruction? (car input))) 
-    (values (exec-form input env) env)]
-   [else 'error]))
-
 ;; Prints objects out to stdout
 (define (print-obj obj)
   (cond
--- vm.scm
+++ vm.scm
@@ -3,12 +3,51 @@
 ;;;; Contains virtual machine constructs and instructions.
 ;;;; Author: Kris Healy
 
+;; Executes a form.  Input is expected to be a list with the executable symbol
+;; being the first element.  All other elements in input are evaluated and
+;; placed on the environment stack.
+(define (exec-form input env)
+  (for-each (lambda (x) (exec-instruction! env 'push (vm-eval x env)))
+	    (reverse (cdr input)))
+  (exec-instruction! env 'push (length (cdr input))) ; Length of arguments
+  (exec-instruction! env (car input))
+  (exec-instruction! env 'pop))
+
+;; Stolen from: http://pointlessprogramming.wordpress.com/2011/02/24/lispy-in-chicken-self-evaluating-values/
+(define (self-evaulating? expr)
+  (or (string? expr)
+      (number? expr)
+      (null? expr)
+      (char? expr)
+      (boolean? expr)))
+
+;; Evaluates an input string
+(define (vm-eval input env)
+  (cond
+   ; Semi-hack used to exit the repl
+   [(and (symbol? input)
+	 (equal? input 'quit)) (values 'quit env)]   
+   [(self-evaulating? input) (values input env)]
+   [(symbol? input)  (values (env/get-def env input) env)]
+   
+   ; Process define forms
+   [(and (list? input)
+         (define-form? input))
+    (define-form input env)]
+
+   ; Process execution of built-in instructions
+   [(and (list? input)
+	 (instruction? (car input))) 
+    (values (exec-form input env) env)]
+   
+   [else 'error]))
+
 ;;; All instructs take the environment as the first argument and an optional
 ;;; second argument.  All return 1 argument, except pop which will return a
 ;;; result as well as the environment.
 
 (define instruction-symbols
-  '(+ push pop - * /))
+  '(+ push pop - * / call))
 
 (define (instruction? sym)
   (any (lambda (x) (equal? x sym))
@@ -33,14 +72,20 @@
   (values (env/pop! env) env))
 
 (define (instruction/arithmetic env op)
-  ; Pop the numbers off the stack in reverse order.  This is important
-  ; for subtraction and division because they aren't communitive!
-  (let* ((num2 (env/pop! env))
- 	 (num1 (env/pop! env)))
-    (env/push! env
-	       (case op
-		 [(+) (+ num1 num2)]
-		 [(-) (- num1 num2)]
-		 [(*) (* num1 num2)]
-		 [(/) (/ num1 num2)]))))
-
+  (let ((arg-length (env/pop! env)))
+    (let* ((num1 (if (> arg-length 1) (env/pop! env) 0))
+           (num2 (if (> arg-length 0) (env/pop! env) 0)))
+      (env/push! env
+                 (case op
+                   [(+) (+ num1 num2)]
+                   [(-) (- num1 num2)]
+                   [(*) (* num1 num2)]
+                   [(/) (/ num1 num2)]))
+      ; Now the result is on the stack.  If we still have arguments
+      ; to add then re-iterate the entire process
+      (if (> arg-length 2)
+          (begin
+            (env/push! env (- arg-length 1))
+            (instruction/arithmetic env op))
+          ; No more arguments - just ignore this case
+          #f))))
